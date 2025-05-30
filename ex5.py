de96d8fe18b878b1a8d46541029d39ed5e936eed
Sruthi Koduri <165888069+SruthiKoduri@users.noreply.github.com>
2025-05-30 15:35:02 +0530

    Number logics and codeforces problems

---
+++ ex5.py
@@ -0,0 +1,2 @@
+n,m=map(int,input("enter the nums").split())
+print(n//m+n%m)
---
+++ recursion.py
@@ -0,0 +1,8 @@
+def fun(a):
+    if a==0:
+        return 0
+    print(x-(a-1))
+    fun(a-1) 
+n=int(input("Enter the number:"))
+x=n
+fun(n)
